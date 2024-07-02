7f781d19719b0724f6a1c0983246747426ae0c3d
Sarita Nandal <sarita.n@breachlock.com>
2024-07-02 15:09:13 +0530

    added files - first version

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'Sarita Nandal',
+    author_email= 'sarita@ma.iitr.ac.in',
+    packages= find_packages(),
+    install_requires = []
+
+)
