854c85409b54c1fe128056c8e8dacd7be6f4e975
Sarita <126271555+SaritaPhD@users.noreply.github.com>
2024-11-28 16:16:44 +0530

    Update store_index.py

--- store_index.py
+++ store_index.py
@@ -19,7 +19,6 @@ embeddings = download_hugging_face_embeddings()
 pinecone.init(api_key=PINECONE_API_KEY,
               environment=PINECONE_API_ENV)
 
-
 index_name="medical-bot"
 
 #Creating Embeddings for Each of The Text Chunks & storing
